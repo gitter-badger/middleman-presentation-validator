dafbdece1c755354066321f9863f7b3cf1ed02c5
Max Meyer <dev@fedux.org>
2015-01-09 23:02:14 +0100

    Allow a callback url to be defined

--- app/controllers/build_jobs_controller.rb
+++ app/controllers/build_jobs_controller.rb
@@ -96,7 +96,7 @@ class BuildJobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def build_job_params
-      params.require(:build_job).permit(:source_file, :add_static_servers)
+      params.require(:build_job).permit(:source_file, :add_static_servers, :callback_url)
     end
 end
 
